1fcd50ec62cb4baaa820d5904e5045ff79b8c820
mostfa29 <dhouib2912@gmail.com>
2022-07-14 11:22:04 +0000

    create_menu_item_component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import MenuItem from './components/header/menuItem/MenuItem';
 
 function App() {
   return (
     <div className="app">
+
+      <MenuItem>
+        <p style={{ margin: 0 }}>test</p>
+      </MenuItem>
 
     </div>
   );
---
+++ src/components/header/menuItem/MenuItem.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components'
+import { useLayoutEffect, useEffect, useRef, useState } from 'react';
+import { Margin } from '@mui/icons-material';
+
+
+function MenuItem(props) {
+
+    const ref = useRef(null);
+
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    const [hover, setHover] = useState(false)
+
+
+
+    //set container width and height to its child width and height
+    useLayoutEffect(() => {
+        setWidth(ref.current.offsetWidth);
+        setHeight(ref.current.offsetHeight);
+    }, [props.children]);
+
+
+    //hover action
+    useEffect(() => {
+        if (hover)
+            console.log('hovered')
+        console.log(`width:${width}/height:${height}`)
+
+
+    }, [hover])
+
+
+
+
+    //handle hover event
+    const MouseEnter = () => {
+        setHover(true)
+    }
+    const MouseLeave = () => {
+        setHover(false)
+    }
+
+
+
+
+
+
+
+    return (
+        <Container
+            onMouseEnter={() => MouseEnter()}
+            onMouseLeave={() => MouseLeave()}
+            //margin is bugy use padding instead
+            style={{
+                width: (width > 70) ? width : '70px',
+                height: (height > 45) ? height : '45px',
+                padding: props.p > 0 ? (props.p + props.pu) : '0.1px',
+                margin: '0'
+
+
+
+
+            }}
+        >
+
+            <Elem ref={ref}> {props.children}</Elem>
+
+
+
+        </Container>
+    )
+}
+
+export default MenuItem
+const Container = styled.div`
+align-items:center;
+text-align:center;
+display: flex;
+flex-direction: column;
+justify-content: center;
+
+
+
+&:hover{
+    cursor:pointer;
+     border:${props => (props.br_w ? props.br_w : '1px ') + 'solid ' + (props.br_c ? props.br_c : ' white')};
+
+}
+`
+
+const Elem = styled.div`
+display:block
+`
